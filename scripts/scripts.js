16a2b69b4a0e69104ffca4f3096d45d587acaedf
Olu Adeyemo <theoafactor@gmail.com>
2018-09-29 16:06:39 +0100

    New additions

---
+++ scripts/scripts.js
@@ -0,0 +1,51 @@
+/**
+ * Using scripts...
+ */
+
+const users = [
+			{
+				'username': 'oluade',
+				'password': 'password'
+			},
+
+			{
+				'username': 'vaughan',
+				'password': 'password'
+			},
+
+			{
+				'username': 'jeff',
+				'password': 'password'
+			}
+	
+	];
+
+
+
+function loginUser(){
+
+const username =document.getElementById('username_id').value;
+
+const password = document.getElementById('password_id').value;
+
+//cHECKING INPUTS
+if(username.length < 3){
+	alert("Please enter a valid username");
+}else{
+	users.forEach((user)=>{
+	if(user['username'] == username){
+		console.log("Yea..Continue");
+	}
+})
+}
+
+
+
+
+return false;
+
+
+}
+
+
+
---
+++ scripts/test.js
@@ -0,0 +1,7 @@
+getMyName('Olumide');
+ function getMyName(myName){
+	return myName;
+}
+
+var myName = getMyName('Olumide');
+
